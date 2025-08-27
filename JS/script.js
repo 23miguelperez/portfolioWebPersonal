8fc939a6d251ed4b22fb572ed47c5fb6c660b6d8
23miguelperez <23miguelperz@gmail.com>
2025-08-27 14:45:24 +0200

    27082025

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,22 @@
 
-
+//MENU DE NAVEGACION  
 const btnHamburguesa = document.getElementById('hamburguesa');
 const nav = document.getElementById('nav');
 
 btnHamburguesa.addEventListener('click', () => {
   nav.classList.toggle('activo');
 });
+
+
+//COOKIES
+const popup = document.getElementById("cookiePopup");
+const btn = document.getElementById("acceptCookies");
+
+if (!localStorage.cookiesAccepted){
+  popup.classList.add("show");
+}
+
+btn.addEventListener('click', function(){
+  localStorage.cookiesAccepted = "true";
+  popup.classList.remove("show")
+})
